ca53e64ebd899c6c58113d6fc4fda2c26bc67f5e
Documenter.jl <documenter@juliadocs.github.io>
2021-05-17 05:44:25 +0000

    build based on c05fcee

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = VectorizedStatistics","category":"page"},{"location":"#VectorizedStatistics","page":"Home","title":"VectorizedStatistics","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for VectorizedStatistics.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [VectorizedStatistics]","category":"page"}]
+}
