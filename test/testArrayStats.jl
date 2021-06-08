40ffceef8e6b43ffad06578fd3dde4bd61b4980a
C.Brenhin Keller <cbkeller@dartmouth.edu>
2021-06-08 17:46:01 -0400

    Try different way of eval-ing in tests

--- test/testArrayStats.jl
+++ test/testArrayStats.jl
@@ -4,30 +4,31 @@
     vreducables = zip((:minimum, :maximum, :sum), (:vminimum, :vmaximum, :vsum))
     for nd = 1:5
         # Generate random array
-        A = rand((10 .+ (1:nd))...)
+        let A = rand((10 .+ (1:nd))...)
 
-        # Test equivlalence when reducing over all dims
-        for (fn, vfn) in vreducables
-            @info "Testing $vfn: $nd-dimensional arrays"
-            @eval @test $vfn($A) ≈ $fn($A)
-        end
-
-        # Test equivalence when reducing over a single dimension
-        for i = 1:nd
+            # Test equivlalence when reducing over all dims
             for (fn, vfn) in vreducables
-                @info "Testing $vfn: reduction over dimension $i"
-                @eval @test $vfn($A, dims=$i) ≈ $fn($A, dims=$i)
+                @info "Testing $vfn: $nd-dimensional arrays"
+                eval(:(@test $vfn($A) ≈ $fn($A);))
             end
-        end
 
-        # Test equivalence when reducing over two dimensions
-        if nd > 1
-            for i = 2:nd
-                for j = 1:i-1
-                    dims = (j,i)
-                    for (fn, vfn) in vreducables
-                        @info "Testing $vfn: reduction over dimensions $dims"
-                        @eval @test $vfn($A, dims=$dims) ≈ $fn($A, dims=$dims)
+            # Test equivalence when reducing over a single dimension
+            for i = 1:nd
+                for (fn, vfn) in vreducables
+                    @info "Testing $vfn: reduction over dimension $i"
+                    eval(:(@test $vfn($A, dims=$i) ≈ $fn($A, dims=$i);))
+                end
+            end
+
+            # Test equivalence when reducing over two dimensions
+            if nd > 1
+                for i = 2:nd
+                    for j = 1:i-1
+                        dims = (j,i)
+                        for (fn, vfn) in vreducables
+                            @info "Testing $vfn: reduction over dimensions $dims"
+                            eval(:(@test $vfn($A, dims=$dims) ≈ $fn($A, dims=$dims);))
+                        end
                     end
                 end
             end
