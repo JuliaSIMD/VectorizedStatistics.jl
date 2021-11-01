b5629acc7b5b423b270ab49a822feca24ac04d1f
C.Brenhin Keller <cbkeller@dartmouth.edu>
2021-11-01 15:14:17 -0400

    Add docstrings for vsort

--- src/vsort.jl
+++ src/vsort.jl
@@ -1,6 +1,49 @@
 ## ---
 
+"""
+```julia
+vsort(A; dims, multithreaded=:auto)
+```
+Return a copy of the array `A`, optionally along dimensions specified by `dims`.
+
+See also `vsort!` for a more efficient in-place version.
+
+## Examples
+```julia
+julia> using VectorizedStatistics
+
+julia> A = [1, 3, 2, 4];
+
+julia> vsort(A)
+4-element Vector{Int64}:
+ 1
+ 2
+ 3
+ 4
+"""
 vsort(A; dims=:, multithreaded=:auto) = vsort!(copy(A), dims=dims, multithreaded=multithreaded)
+
+
+"""
+```julia
+vsort!(A; dims, multithreaded=:auto)
+```
+Sort the array `A`, optionally along dimensions specified by `dims`.
+
+## Examples
+```julia
+julia> using VectorizedStatistics
+
+julia> A = [1, 3, 2, 4];
+
+julia> vsort!(A)
+4-element Vector{Int64}:
+ 1
+ 2
+ 3
+ 4
+```
+"""
 function vsort!(A; dims=:, multithreaded=:auto)
     if (multithreaded==:auto && length(A) > 16383) || multithreaded==true
         _vtsort!(A, dims)
